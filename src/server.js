4817228d75ce99f9a128b7f47eb835ebc180f47d
walid horchani <walid.horchani@oyez.fr>
2020-09-18 10:09:36 +0100

    refact server

--- src/server.js
+++ src/server.js
@@ -2,19 +2,16 @@ const { GraphQLServer } = require('graphql-yoga');
 
 require('dotenv').config();
 
-const Query = require('./resolvers/Query');
-const Mutation = require('./resolvers/Mutation');
 const { connect } = require('./database/utils');
 
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 //database mongodb connection
 connect();
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers: {
-    Query,
-    Mutation,
-  },
+  typeDefs,
+  resolvers,
 });
 
 server.start(() => console.log('Server is running on localhost:4000'));
